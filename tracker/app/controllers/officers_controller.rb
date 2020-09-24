c5c80713b793ceb9c9926e7da9010f0db0c90d2d
Nikolai Thompson <y-yuss@tamu.ed>
2020-09-23 19:14:41 -0500

    adde functioning login page

--- tracker/app/controllers/officers_controller.rb
+++ tracker/app/controllers/officers_controller.rb
@@ -12,6 +12,27 @@ class OfficersController < ApplicationController
         end
     end
 
+    def login
+        
+    end
+
+    def attemptLogin
+        if params[:email].present? && params[:password].present?
+            currentUser = Officer.where(:email => params[:email]).first
+            if currentUser && currentUser.authenticate(params[:password])
+                session[:userId] = currentUser.id
+                flash[:notice] = "Success logging in!"
+                redirect_to(new_officer_path) #This should be changed to whatever the home page is later
+            else
+                flash.now[:notice] = "Invalid username or password."
+                render('login')
+            end
+        else
+            flash.now[:notice] = "Please fill in your info. "
+            render('login')
+        end
+    end
+
     def officerParams
         params.require(:officer).permit(:name, :email, :position, :UIN, :password)
     end
--- tracker/config/routes.rb
+++ tracker/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "officers#new"
+  root "officers#login"
   
+  get "officers/login"
+  post "officers/attemptLogin"
   resources :officers do
     member do
       get :delete
