5e72a0a1099d1411635f606e78f97d3f57879dd4
Jesus Cedillo <jcedillo269@gmail.com>
2020-10-06 19:28:50 -0500

    can export member table to csv

--- tracker/app/models/member.rb
+++ tracker/app/models/member.rb
@@ -1,2 +1,16 @@
 class Member < ApplicationRecord
+
+    def self.to_csv
+        attributes = %w{name email uin points}
+
+        CSV.generate(headers: true) do |csv|
+            csv << attributes
+
+            all.each do |member|
+                #csv << attributes.map{ |attr| member.send(attr) }
+                csv << member.attributes.values_at(*attributes)
+            end
+        end
+    end
+
 end
